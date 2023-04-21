c10fbbe048eda1080e1f6d3024ff8850f2b0e0df
Adi13131313 <126593561+Adi13131313@users.noreply.github.com>
2023-04-21 16:20:28 +0200

    création données détentions

---
+++ donnees_detentions.js
@@ -0,0 +1,43 @@
+//donnees detentions
+[
+    {
+        id : 1
+        annee: 2016,
+        places_disponibles: 7483,
+        places_occupees: 6878,
+        femmes:383,
+        hommes: 6495
+    },
+    {
+        id :2,
+        annee: 2017
+        places_disponibles:7489,
+        places_occupees: 6907,
+        femmes: 382,
+        hommes: 6525
+    },
+    {
+        id:3,
+        annee: 2019,
+        places_disponibles:7394,
+        places_occupees: 6943,
+        femmes: 396
+        hommes: 6547
+    },
+    {
+        id:4,
+        annee:2020,
+        places_disponibles:7373,
+        places_occupees: 6897,
+        femmes: 401,
+        hommes: 6496
+    },
+    {
+        id:5,
+        annnee:2021,
+        places_disponibles:7397,
+        places_occupees:6316,
+        femmes:357,
+        hommes: 5959
+    }
+]
